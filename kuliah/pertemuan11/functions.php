d99761cd28767f75f2c9d66743ca4a251f1ff622
Fazril235 <arielff54@gmail.com>
2021-04-27 15:01:51 +0700

    menyelesaikan video pertemuan 11

---
+++ kuliah/pertemuan11/functions.php
@@ -0,0 +1,97 @@
+<?php
+/*
+Muhammad Fazril
+203040068
+https://github.csom/Fazril235/pw2021_203040068
+Pertemuan 10
+*/
+?>
+<?php
+
+function Koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'phpdasar');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+
+  $result = mysqli_query($conn, $query);
+
+
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
+
+function tambah($data)
+{
+  $conn = koneksi();
+
+  $nama = htmlspecialchars($data['Nama']);
+  $nrp = htmlspecialchars($data['Nrp']);
+  $email = htmlspecialchars($data['Email']);
+  $jurusan = htmlspecialchars($data['Jurusan']);
+  $gambar = htmlspecialchars($data['Gambar']);
+
+  $query = "INSERT INTO
+              mahasiswa
+            VALUES
+            (null, '$nama', '$nrp', '$email', '$jurusan', '$gambar');
+            ";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function hapus($id)
+{
+  $conn = koneksi();
+  mysqli_query($conn, "DELETE FROM mahasiswa WHERE id = $id") or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function ubah($data)
+{
+  $conn = koneksi();
+
+  $id = $data['id'];
+  $nama = htmlspecialchars($data['Nama']);
+  $nrp = htmlspecialchars($data['Nrp']);
+  $email = htmlspecialchars($data['Email']);
+  $jurusan = htmlspecialchars($data['Jurusan']);
+  $gambar = htmlspecialchars($data['Gambar']);
+
+  $query = "UPDATE mahasiswa SET
+              Nama = '$nama',
+              Nrp = '$nrp',
+              Email = '$jurusan',
+              Gambar = '$gambar'
+            WHERE id = $id";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function cari($keyword)
+{
+  $conn = koneksi();
+
+  $query = "SELECT * FROM mahasiswa
+              WHERE 
+              Nama LIKE '%$keyword%' OR 
+              Nrp LIKE '%$keyword%'
+              ";
+
+  $result = mysqli_query($conn, $query);
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
---
+++ kuliah/pertemuan11/ubah.php
@@ -0,0 +1,83 @@
+<?php
+require 'functions.php';
+
+// jika tidak ada id di url
+if (!isset($_GET['id'])) {
+  header("Location: index.php");
+  exit;
+}
+
+// ambil id dari url
+$id = $_GET['id'];
+
+// query mahasiswa berdasarkan id
+$m = query("SELECT * FROM mahasiswa WHERE id = $id");
+
+
+// cek apakah tombol ubah sudah ditekan
+if (isset($_POST['ubah'])) {
+  if (ubah($_POST) > 0) {
+    echo "<script>
+            alert('data berhasil diubah');
+            document.location.href = 'index.php';
+            </script>";
+  } else {
+    echo "data gagal diubah";
+  }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ubah Data Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Form Ubah Data Mahasiswa</h3>
+  <form action="" method="POST">
+    <input type="hidden" name="id" value="<?= $m['id']; ?>">
+    <ul>
+      <li>
+        <label>
+          Nama :
+          <input type="text" name="Nama" autofocus required value="<?= $m['Nama']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          NRP :
+          <input type="text" name="Nrp" required value="<?= $m['Nrp']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Email :
+          <input type="text" name="Email" required value="<?= $m['Email']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Jurusan :
+          <input type="text" name="Jurusan" required value="<?= $m['Jurusan']; ?>">
+        </label>
+      </li>
+      <li>
+        <label>
+          Gambar :
+          <input type="text" name="Gambar" required value="<?= $m['Gambar']; ?>">
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="ubah">Ubah Data!</button>
+      </li>
+    </ul>
+  </form>
+</body>
+
+</html>
